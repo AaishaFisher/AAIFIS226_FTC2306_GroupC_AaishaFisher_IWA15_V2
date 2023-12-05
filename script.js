4464ada743a527cc27676822dc05da81f2bb786e
Aai'sha Fisher <aaishafisher484@gmail.com>
2023-12-05 07:07:23 +0200

    Complete

--- script.js
+++ script.js
@@ -1,49 +1,52 @@
 const data = {
-	lists: [
-		['first', [15, 11, 13, 7, 5]],
-		['second', [2, 6, 8, 4, 14, 12, 10]],
-		['third', [9, 3, 1]],
-	]
-}
-
-// Only edit below
-
-const { first = 1 } = data.first || {}
-const { second = 1 } = data.second || {}
-const { third = 1 } = data.third || {}
-
-const result = []
-
-const extractBiggest = () => {
-	if (first[-1] > second[-1]) {
-		return first
-	}
-
-	if (third[-1] < 1) {
-		return second
-	}
-	
-	return third
-}
-
-// Only edit above
-
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-
-console.log(result)
+    lists: [
+      ['first', [15, 11, 13, 7, 5]],
+      ['second', [2, 6, 8, 4, 14, 12, 10]],
+      ['third', [9, 3, 1]],
+    ]
+  };
+  
+  // Only edit below
+  
+  const result = [];
+  
+  const extractBiggest = () => {
+    const { lists } = data;
+    const indices = lists.map(([, arr]) => arr[arr.length - 1]);
+  
+    const availableIndices = indices.filter(val => !isNaN(val) && val !== undefined);
+    
+    if (availableIndices.length > 0) {
+      const maxIndex = indices.indexOf(Math.max(...availableIndices));
+  
+      if (lists[maxIndex][1].length > 0) {
+        return lists[maxIndex][1].pop();
+      } else {
+        lists.splice(maxIndex, 1);
+        return extractBiggest();
+      }
+    }
+  };
+  
+  // Only edit above
+  
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  
+  console.log(result);
+  
